a509c8a60c711f259255db5f9afe2c8f1ad1ac5b
Oleg <morgunoleg2002@gmail.com>
2022-09-11 23:26:45 +0300

    gud work

---
+++ Geek_les_1.py
@@ -0,0 +1,57 @@
+import os
+import psutil
+import pprint
+import sys
+import shutil
+quastion = ''
+while quastion != 'w':
+    quastion = input("Хотите ли вы поработать?(Y/N/w)")
+    if quastion == "Y":
+        print("Что именно вы совуршить?")
+        print("[1] - показать содержимое папки?")
+        print("""[2] - вывести информацию о системе:
+                 Имя текущей дерриктории;
+                 Тип операционной системы;
+                 Кодировку файловой системы;
+                 Количество CPU;
+                 Логин пользователя
+        """)
+        print("[3] - выведу список процессов")
+        print("[4] - сделаю копию файлов имеющихся в текущей дерриктории")
+        print("[5] - продублирую файл, имя которого укажете")
+        print("[6] - удалю все файлы с окончанием '.dupl' в указанной вами дирректории")
+        choice = int(input("Выберите один из вариантов:"))
+        if choice == 1:
+            print(os.listdir())
+        if choice == 2:
+            print(os.getcwd())
+            print(sys.platform)
+            print(sys.getdefaultencoding())
+            print(psutil.cpu_count())
+            print(os.getlogin())
+        if choice == 3:
+            pprint.pprint(psutil.pids())
+        if choice == 4:
+            i = 0
+            list = os.listdir()
+            while i < len(list):
+                new_files = list[i] + ".dubl"
+                shutil.copy(list[i], new_files)
+                i += 1
+        if choice == 5:
+            file_name = input()
+            fn_dupl = file_name + ".dupl"
+            shutil.copy(file_name, fn_dupl)
+        if choice == 6:
+            change_dir = input()
+            os.chdir(change_dir)
+            data = os.listdir()
+            for i in range(len(data)):
+                if data[i].endswith(".dupl") is True:
+                    os.remove(data[i])
+                else:
+                    continue
+    elif quastion == "N":
+        print("Мужик я тобою не доволен!")
+    else:
+        pass
